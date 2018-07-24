e35677993a7b9ba6c8871fcdcaac3ac8a9cb2e66
markdenverPH <babaranmark@yahoo.com>
2018-07-24 22:36:34 +0800

    edited dbase
    -complete

--- database/dbase_WITH_DATA.sql
+++ database/dbase_WITH_DATA.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 24, 2018 at 03:53 PM
+-- Generation Time: Jul 24, 2018 at 04:36 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.2
 
@@ -25,40 +25,56 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `settings`
+-- Table structure for table `reservation`
 --
 
-CREATE TABLE `settings` (
-  `settings_id` int(11) NOT NULL,
-  `settings_tax` int(10) NOT NULL
+CREATE TABLE `reservation` (
+  `reservation_id` int(11) NOT NULL,
+  `reservation_in` bigint(11) NOT NULL,
+  `reservation_out` bigint(11) NOT NULL,
+  `reservation_adult` bigint(11) NOT NULL,
+  `reservation_child` bigint(11) NOT NULL,
+  `reservation_roomCount` bigint(11) NOT NULL,
+  `reservation_day_type` bigint(11) NOT NULL,
+  `reservation_status` bigint(11) NOT NULL,
+  `reservation_key` varchar(255) NOT NULL,
+  `reservation_requests` text,
+  `guest_id` int(11) NOT NULL,
+  `room_type_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `settings`
---
-
-INSERT INTO `settings` (`settings_id`, `settings_tax`) VALUES
-(1, 12);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `settings`
+-- Indexes for table `reservation`
 --
-ALTER TABLE `settings`
-  ADD PRIMARY KEY (`settings_id`);
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`reservation_id`),
+  ADD KEY `fk_reservation_guest1_idx` (`guest_id`),
+  ADD KEY `fk_reservation_room_type1_idx` (`room_type_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `settings`
+-- AUTO_INCREMENT for table `reservation`
 --
-ALTER TABLE `settings`
-  MODIFY `settings_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `reservation`
+  MODIFY `reservation_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD CONSTRAINT `fk_reservation_guest1` FOREIGN KEY (`guest_id`) REFERENCES `guest` (`guest_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_reservation_room_type1` FOREIGN KEY (`room_type_id`) REFERENCES `room_type` (`room_type_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
