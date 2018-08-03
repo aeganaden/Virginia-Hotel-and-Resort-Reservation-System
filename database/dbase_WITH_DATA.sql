d1ae393843eb01fc031378c4dfa0493a41e135a4
markdenverPH <babaranmark@yahoo.com>
2018-08-03 22:33:34 +0800

    updated WITH_DATA

--- database/dbase_WITH_DATA.sql
+++ database/dbase_WITH_DATA.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 03, 2018 at 04:16 PM
+-- Generation Time: Aug 03, 2018 at 04:33 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.2
 
@@ -21,6 +21,61 @@ SET time_zone = "+00:00";
 --
 -- Database: `virginia`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `billing`
+--
+
+CREATE TABLE `billing` (
+  `billing_id` int(11) NOT NULL,
+  `billing_price` bigint(11) NOT NULL,
+  `billing_name` varchar(225) NOT NULL,
+  `billing_quantity` bigint(11) NOT NULL,
+  `reservation_key` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guest`
+--
+
+CREATE TABLE `guest` (
+  `guest_id` int(11) NOT NULL,
+  `guest_firstname` varchar(45) NOT NULL,
+  `guest_lastname` varchar(45) NOT NULL,
+  `guest_gender` varchar(10) NOT NULL,
+  `guest_phone` bigint(11) NOT NULL,
+  `guest_address` text NOT NULL,
+  `guest_email` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `reservation_id` int(11) NOT NULL,
+  `reservation_in` bigint(11) NOT NULL,
+  `reservation_out` bigint(11) NOT NULL,
+  `reservation_adult` bigint(11) NOT NULL,
+  `reservation_child` bigint(11) NOT NULL,
+  `reservation_roomCount` bigint(11) NOT NULL,
+  `reservation_day_type` bigint(11) NOT NULL,
+  `reservation_status` bigint(11) NOT NULL,
+  `reservation_key` varchar(255) NOT NULL,
+  `reservation_reserved_at` bigint(15) NOT NULL,
+  `reservation_updated_at` bigint(15) NOT NULL,
+  `reservation_payment_status` tinyint(1) NOT NULL,
+  `reservation_payment_photo` text NOT NULL,
+  `reservation_requests` text,
+  `guest_id` int(11) NOT NULL,
+  `room_type_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -67,9 +122,70 @@ INSERT INTO `room` (`room_id`, `room_name`, `room_status`, `room_type_id`, `rese
 (24, 'D15', 3, 2, NULL),
 (25, 'D16', 3, 2, NULL);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room_type`
+--
+
+CREATE TABLE `room_type` (
+  `room_type_id` int(11) NOT NULL,
+  `room_type_name` varchar(255) NOT NULL,
+  `room_type_description` text NOT NULL,
+  `room_type_pax` int(10) NOT NULL,
+  `room_type_image` varchar(225) NOT NULL,
+  `room_type_price` bigint(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `room_type`
+--
+
+INSERT INTO `room_type` (`room_type_id`, `room_type_name`, `room_type_description`, `room_type_pax`, `room_type_image`, `room_type_price`) VALUES
+(1, 'Standard Room', 'This room is fully airconditioned room with television and a best fit for 2 person.', 2, 'room1.jpg', 1500),
+(2, 'Double Bedroom', 'This room is a fully airconditioned room with television and best fit for 4 person.', 4, 'room2.jpg', 2000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+CREATE TABLE `settings` (
+  `settings_id` int(11) NOT NULL,
+  `settings_tax` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`settings_id`, `settings_tax`) VALUES
+(1, 12);
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `billing`
+--
+ALTER TABLE `billing`
+  ADD PRIMARY KEY (`billing_id`);
+
+--
+-- Indexes for table `guest`
+--
+ALTER TABLE `guest`
+  ADD PRIMARY KEY (`guest_id`);
+
+--
+-- Indexes for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`reservation_id`),
+  ADD KEY `fk_reservation_guest1_idx` (`guest_id`),
+  ADD KEY `fk_reservation_room_type1_idx` (`room_type_id`);
 
 --
 -- Indexes for table `room`
@@ -80,8 +196,38 @@ ALTER TABLE `room`
   ADD KEY `fk_room_reservation1_idx` (`reservation_id`);
 
 --
+-- Indexes for table `room_type`
+--
+ALTER TABLE `room_type`
+  ADD PRIMARY KEY (`room_type_id`);
+
+--
+-- Indexes for table `settings`
+--
+ALTER TABLE `settings`
+  ADD PRIMARY KEY (`settings_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `billing`
+--
+ALTER TABLE `billing`
+  MODIFY `billing_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `guest`
+--
+ALTER TABLE `guest`
+  MODIFY `guest_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `reservation_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `room`
@@ -90,8 +236,27 @@ ALTER TABLE `room`
   MODIFY `room_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
+-- AUTO_INCREMENT for table `room_type`
+--
+ALTER TABLE `room_type`
+  MODIFY `room_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `settings`
+--
+ALTER TABLE `settings`
+  MODIFY `settings_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD CONSTRAINT `fk_reservation_guest1` FOREIGN KEY (`guest_id`) REFERENCES `guest` (`guest_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_reservation_room_type1` FOREIGN KEY (`room_type_id`) REFERENCES `room_type` (`room_type_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `room`
