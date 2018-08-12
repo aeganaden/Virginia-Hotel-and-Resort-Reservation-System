4e5f7b7ec58b55570129260fab5a3d064664d216
markdenverPH <markdenverbabaran@gmail.com>
2018-08-12 16:35:43 +0800

    added some and fixes
    -added composer
    -added mpdf
    -added other "require" in composer.json

--- application/controllers/Payments.php
+++ application/controllers/Payments.php
@@ -146,11 +146,28 @@ class Payments extends CI_Controller {
 
 
 	}
-	
 
 	public function downloadPDF(){
-		$input = $this->input->post(NULL, TRUE);
-		echo json_encode($input);
+		require_once './application/vendor/autoload.php';
+		$data = array(
+			"title"=> "PDF",
+			"test" => "HELLO WORLD"
+		);
+		$mpdf = new \Mpdf\Mpdf();
+
+		// Buffer the following html with PHP so we can store it to a variable later
+		ob_start();
+
+		// This is where your script would normally output the HTML using echo or print
+		$this->load->view('pdf/pdf_file', $data); //last-mark
+
+		// Now collect the output buffer into a variable
+		$html = ob_get_contents();
+		ob_end_clean();
+
+		// send the captured HTML from the output buffer to the mPDF class for processing
+		$mpdf->WriteHTML($html);
+		$mpdf->Output();
 	}
 
 }
---
+++ application/views/pdf/pdf_file.php
@@ -0,0 +1,58 @@
+
+<html>
+<head>
+	<title><?=$title?></title>
+	<style>
+	.bord1{
+		border:solid 2px  black;
+	}
+	img {
+		height:auto;
+		width: 19%;
+	}
+	.header{
+		text-align: center;
+	}
+	.center_text{
+		text-align: center;
+	}
+	body{
+		font-family:chelvetica;
+	}
+	.body{
+		font-family:courier;
+		border-top: solid 1px black;
+		border-bottom: solid 1px black;
+	}
+	.resort{
+		font-weight: bold;
+		font-size: 20px;
+	}
+	.footer {
+		
+	}
+</style>
+</head>
+<body>
+
+	<div class="header">
+		<img src="<?=base_url()?>assets/images/11.gif"/><br>
+		<span class="resort">Virginia & Boy Lodge and Resort</span> <br>
+		502 M.H. Del Pilar ST. Brgy. San Rafael Montalban Rizal
+	</div><br>
+
+	<div class="body">
+		<span>Payments: </span>
+	</div>
+	
+	
+	<div class="footer"> <!-- footer -->
+		<span style="font-weight: bold;font-size: 17px;">Virginia & Boy Lodge and Resort</span><br>
+		Address: 502 M.H. Del Pilar ST. Brgy. San Rafael Montalban Rizal<br>
+		Tel: 6669591 / 9852943 / 5708584<br>
+		Mobile: 09087404449<br>
+		E-mail: Virginia&Boy@gmail.com<br>
+	</div>
+</body>
+
+</html>
